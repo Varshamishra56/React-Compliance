58cc9fa2a4a788caf40b595b96aceedf5316ae9e
Varshamishra56 <vm0222912@gmail.com>
2024-07-20 23:03:27 +0530

    useeffect

--- SocialMedia/src/Components/PostList.jsx
+++ SocialMedia/src/Components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../Store/post-list-store";
 import WelcomeMsg from "./WelcomeMsg";
@@ -6,19 +6,26 @@ import WelcomeMsg from "./WelcomeMsg";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
-  const handlePostList = () => {
+  // const [datafetched, setDataFetched] = useState(false);
+  // if (!datafetched) {
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       addInitialPosts(data.posts);
+  //     });
+  //   setDataFetched(true);
+  // }
+
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
-
+  }, []);
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMsg onGetPostsClick={handlePostList}></WelcomeMsg>
-      )}
+      {postList.length === 0 && <WelcomeMsg></WelcomeMsg>}
       {postList.map((post) => (
         <Post key={post.id} post={post}></Post>
       ))}
--- SocialMedia/src/Components/WelcomeMsg.jsx
+++ SocialMedia/src/Components/WelcomeMsg.jsx
@@ -2,13 +2,6 @@ const WelcomeMsg = ({ onGetPostsClick }) => {
   return (
     <center>
       <h1 className="welcomeMsg">There are no posts to display</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostsClick}
-      >
-        Get Posts From Server
-      </button>
     </center>
   );
 };
